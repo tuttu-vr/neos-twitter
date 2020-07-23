3a5d7cc0f435f49546796e559e0f08f03115e6fe
tuttu <tuttu74459297@gmail.com>
2020-07-23 19:18:06 +0900

    add test and fix api

--- server/lib/twitter.py
+++ server/lib/twitter.py
@@ -1,10 +1,10 @@
 from typing import List, Dict
 from logging import getLogger
-from urllib.parse import urlencode
+from urllib.parse import quote
 
 from common.models.neotter_user import NeotterUser
 from common.models.tweet import Tweet
-from common.models.twitter_user import TwitterUser, get_by_id
+from common.models.twitter_user import TwitterUser
 from common.lib import twitter
 from lib.settings import TWEET_DELIMITER
 
@@ -28,8 +28,12 @@ def _api_by_user(user: NeotterUser):
 def _statuses_to_dict_list(status_list_raw: list, neotter_user_id: str) -> List[Dict]:
     # need to be tested
     # neotter_user_id is not used
-    message_list = map(lambda tw: Tweet.create(tw, neotter_user_id).to_dict(), status_list_raw)
-    user_list = map(lambda us: TwitterUser.create(us).to_dict(), map(lambda st: st.user, status_list_raw))
+    message_list = map(
+        lambda tw: Tweet.create(tw, neotter_user_id, contain_retweet=True).to_dict(),
+        status_list_raw)
+    user_list = map(
+        lambda us: TwitterUser.create(us).to_dict(), map(
+            lambda st: st.user, status_list_raw))
     status_list = []
     for mes, tweet_user in zip(message_list, user_list):
         mes.update(tweet_user)
@@ -47,19 +51,17 @@ def get_status_list(user: NeotterUser, status_id_list_str: str) -> List[Dict]:
 
 
 def get_user_timeline(user: NeotterUser, twitter_user_id: str, count: int=50) -> List[Dict]:
-    twitter_user = get_by_id(twitter_user_id)
     api = _api_by_user(user)
-    user_timeline = api.GetUserTimeline(twitter_user.id, count=50)
+    user_timeline = api.GetUserTimeline(twitter_user_id, count=50)
     return _statuses_to_dict_list(user_timeline, user.id)
 
 
 def get_search_result(user: NeotterUser, search_query: str) -> List[Dict]:
     query = {
-        'q': search_query,
+        'term': search_query,
         'result_type': 'recent',
         'count': 50
     }
-    query_raw = urlencode(query)
     api = _api_by_user(user)
-    search_result = api.GetSearch(query_raw)
+    search_result = api.GetSearch(**query)
     return _statuses_to_dict_list(search_result, user.id)
---
+++ test/combined_test/sample_request_v2.py
@@ -0,0 +1,62 @@
+import json
+import requests
+import argparse
+import time
+from logging import getLogger, DEBUG, StreamHandler
+
+from test_utils import parser
+
+DELIMITER = '$'
+logger = getLogger(__name__)
+logger.setLevel(DEBUG)
+logger.addHandler(StreamHandler())
+endpoint = 'http://localhost:8080/api/v2/'
+
+
+def load_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('key')
+    return parser.parse_args()
+
+
+def dump_request(api_name: str, query: dict):
+    res = requests.get(endpoint + api_name, query)
+    logger.info(api_name)
+    logger.info(res.text)
+    for message_str in res.text.split(DELIMITER):
+        message = parser.parse_message(message_str)
+        logger.info(json.dumps(message, indent=4, ensure_ascii=False))
+
+
+def test_get_status_list(key: str, id_list: list=['1286075295468892160', '1286202545065431042']):
+    status_id_list = ','.join(id_list)
+    query = {
+        'key': key,
+        'status_ids': status_id_list
+    }
+    dump_request('status-list', query)
+
+
+def test_get_user_timeline(key: str, user_id: str='1213118571045240835'):
+    query = {
+        'key': key,
+        'user_id': user_id
+    }
+    dump_request('user-timeline', query)
+
+
+def test_get_search_result(key: str, query: str='#NeosVR'):
+    query = {
+        'key': key,
+        'query': query
+    }
+    dump_request('search-result', query)
+
+
+if __name__ == '__main__':
+    args = load_args()
+    # test_get_status_list(args.key)
+    # time.sleep(0.5)
+    # test_get_user_timeline(args.key)
+    # time.sleep(0.5)
+    test_get_search_result(args.key)
