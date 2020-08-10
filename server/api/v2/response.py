94d3084e295bf27fb80d6ef64b932466ee22927a
tuttu <tuttu74459297@gmail.com>
2020-08-10 10:21:51 +0900

    [fix] fix some error

--- server/api/v2/response.py
+++ server/api/v2/response.py
@@ -66,6 +66,8 @@ def _process_profile(twitter_user_raw: dict):
         'description': 'description'
     }
     user_dict = twitter_user_raw.AsDict()
+    if 'profile_banner_url' not in user_dict:
+        user_dict['profile_banner_url'] = 'https://placehold.jp/1500x500.png'
     result = ';'.join([
         f'{neotter_key}={quote(user_dict[key])}' for key, neotter_key in parameters.items()
     ])
